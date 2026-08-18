c37ae55271d326811ed1b4fda9fbe20544b9b32c
DipuChandraRoy <dipuray7779@gmail.com>
2026-08-18 14:47:30 +0600

    Make Slider Provider

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:multiplestateprovider/sliderprovider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: Text("data"), centerTitle: true),
+      body: Column(
+        children: [
+          Consumer(
+            builder: (context, ref, child) {
+              final slider = ref.watch(SliderProvider);
+
+              return Container(
+                height: 200,
+                width: 200,
+                color: Colors.red.withOpacity(slider),
+              );
+            },
+          ),
+          Consumer(
+            builder: (context, ref, child) {
+              final slider = ref.watch(SliderProvider);
+
+              return Slider(
+                value: slider,
+                onChanged: (value) {
+                  ref.read(SliderProvider.notifier).state = value;
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:multiplestateprovider/home.dart';
 
 void main() {
@@ -11,10 +12,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: HomePage(),
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+        home: HomePage(),
+      ),
     );
   }
 }
